f12eb21d927da00f1bff1e1e7043f1fdfa8c2e99
Daya Chettri <Dayasagar073@gmail.com>
2022-12-19 03:20:57 +0530

    update svg

--- script/navbar.js
+++ script/navbar.js
@@ -5,23 +5,39 @@ function navbar() {
       <div class="navbar-left">
         <a href="./index.html" class="logo"><i class="fa-brands fa-linkedin"></i></a>
         <div class="search-box">
-          <i class="ph-magnifying-glass"></i>
+         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+  <path d="M21.41 18.59l-5.27-5.28A6.83 6.83 0 0017 10a7 7 0 10-7 7 6.83 6.83 0 003.31-.86l5.28 5.27a2 2 0 002.82-2.82zM5 10a5 5 0 115 5 5 5 0 01-5-5z"></path>
+</svg>
           <input type="text" placeholder="Search">
         </div>
       </div>
 
       <div class="navbar-center">
-        <div class="work search"><i class="ph-magnifying-glass"></i><span>Search<span></div>
-        <div class="work"><a href="/pages/home.html"><i class="ph-house-fill"></i><span>Home</span></a></div>
-        <div class="work"><a href="/pages/network.html"><i class="ph-users-three-fill"></i><span class="nav-network">My Network</span></a></div>
-        <div class="work"><a href="/pages/jobs.html"><i class="ph-suitcase-simple-fill"></i><span>Jobs</span></a></div>
-        <div class="work"><a href="/pages/message.html"><i class="ph-chat-teardrop-dots-fill"></i><span>Messaging</span></a></div>
-        <div class="work"><a href="/pages/notification.html"><i class="ph-bell-fill"></i><span>Notifications</span></a></div>
+        <div class="work search"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+  <path d="M21.41 18.59l-5.27-5.28A6.83 6.83 0 0017 10a7 7 0 10-7 7 6.83 6.83 0 003.31-.86l5.28 5.27a2 2 0 002.82-2.82zM5 10a5 5 0 115 5 5 5 0 01-5-5z"></path>
+</svg><span>Search<span></div>
+        <div class="work"><a href="/pages/home.html"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+  <path d="M23 9v2h-2v7a3 3 0 01-3 3h-4v-6h-4v6H6a3 3 0 01-3-3v-7H1V9l11-7 5 3.18V2h3v5.09z"></path>
+</svg><span>Home</span></a></div>
+        <div class="work"><a href="/pages/network.html"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+  <path d="M12 16v6H3v-6a3 3 0 013-3h3a3 3 0 013 3zm5.5-3A3.5 3.5 0 1014 9.5a3.5 3.5 0 003.5 3.5zm1 2h-2a2.5 2.5 0 00-2.5 2.5V22h7v-4.5a2.5 2.5 0 00-2.5-2.5zM7.5 2A4.5 4.5 0 1012 6.5 4.49 4.49 0 007.5 2z"></path>
+</svg><span class="nav-network">My Network</span></a></div>
+        <div class="work"><a href="/pages/jobs.html"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+  <path d="M17 6V5a3 3 0 00-3-3h-4a3 3 0 00-3 3v1H2v4a3 3 0 003 3h14a3 3 0 003-3V6zM9 5a1 1 0 011-1h4a1 1 0 011 1v1H9zm10 9a4 4 0 003-1.38V17a3 3 0 01-3 3H5a3 3 0 01-3-3v-4.38A4 4 0 005 14z"></path>
+</svg><span>Jobs</span></a></div>
+        <div class="work"><a href="/pages/message.html"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+  <path d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z"></path>
+</svg><span>Messaging</span></a></div>
+        <div class="work"><a href="/pages/notification.html"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+  <path d="M22 19h-8.28a2 2 0 11-3.44 0H2v-1a4.52 4.52 0 011.17-2.83l1-1.17h15.7l1 1.17A4.42 4.42 0 0122 18zM18.21 7.44A6.27 6.27 0 0012 2a6.27 6.27 0 00-6.21 5.44L5 13h14z"></path>
+</svg><span>Notifications</span></a></div>
         <div class="online">
           <div class="work"><img src="/img/profile.jpg" class="profile-img"><span>Me</span></div>
         </div>
         <div class="work three-dot"><i class="ph-dots-three"></i></div>
-        <div class="work nine-dots threeDot-content"><i class="ph-dots-nine-bold"></i><span>Work</span></div>
+        <div class="work nine-dots threeDot-content"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+  <path d="M3 3h4v4H3zm7 4h4V3h-4zm7-4v4h4V3zM3 14h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4zM3 21h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4z"></path>
+</svg><p>Work</p></div>
         <div class="work try-premium threeDot-content"><a class="try-premium" href="#">Try Premium for <br>free</a></div>
 
       </div>
